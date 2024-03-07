d81ba7c98d8ca9da3fb99f6f34b13986a45a4e0a
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-03-07 13:52:15 +0300

    first commit

---
+++ control.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  // Prompt the user for a number
+  stdout.write('Enter a number: ');
+  String input = stdin.readLineSync()!;
+  double number = double.tryParse(input) ?? 0;
+
+  // Check the number and print the appropriate message
+  if (number > 10) {
+    print('Your number is greater than 10');
+  } else if (number < 10) {
+    print('Your number is less than 10');
+  } else {
+    print('Your number is equal to 10');
+  }
+}
